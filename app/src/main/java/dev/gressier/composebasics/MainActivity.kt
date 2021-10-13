2006e9ef0cec2543f25877aa211d72cb2dd55df2
Alexandre Gressier <alexandre@gressier.dev>
2021-10-13 20:04:38 +0200

    Make the background color change if an list item is tapped

--- app/src/main/java/dev/gressier/composebasics/MainActivity.kt
+++ app/src/main/java/dev/gressier/composebasics/MainActivity.kt
@@ -3,6 +3,9 @@ package dev.gressier.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +15,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.gressier.composebasics.ui.theme.ComposeBasicsTheme
@@ -70,10 +74,21 @@ fun Counter(count: Int, updateCount: (Int) -> Unit) {
 
 @Composable
 fun Greeting(name: String) {
-    Text(
-        "Hello $name!",
-        Modifier.padding(16.dp)
+    var isSelected by remember {
+        mutableStateOf(false)
+    }
+    val targetColor by animateColorAsState(
+        targetValue = if (isSelected) MaterialTheme.colors.primary else Color.Transparent,
+        animationSpec = tween(durationMillis = 1000),
     )
+    Surface(color = targetColor) {
+        Text(
+            "Hello $name!",
+            Modifier
+                .clickable { isSelected = !isSelected }
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
