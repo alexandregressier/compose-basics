dc1690d53eac4a5e77c30a9452e90f35c4231f04
Alexandre Gressier <alexandre@gressier.dev>
2021-10-13 19:07:06 +0200

    Extract the list of names into its own composable

--- app/src/main/java/dev/gressier/composebasics/MainActivity.kt
+++ app/src/main/java/dev/gressier/composebasics/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
@@ -28,14 +29,23 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyScreenContent(names: List<String> = listOf("Android", "There")) {
     var counterState by remember { mutableStateOf(0) }
-    Column() {
-        names.forEach { name ->
-            Greeting (name = name)
-            Divider()
-        }
+
+    Column(modifier = Modifier.fillMaxHeight()) {
+        NamesList(names = names, modifier = Modifier.weight(1f))
+
         Counter(count = counterState, updateCount = { newCount -> counterState = newCount })
         if (counterState > 5) {
             Text(text = "I ❤️ to count!")
+        }
+    }
+}
+
+@Composable
+fun NamesList(names: List<String>, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        names.forEach { name ->
+            Greeting(name = name)
+            Divider()
         }
     }
 }
