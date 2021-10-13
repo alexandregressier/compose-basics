37ad7abe284a60387db5c034b95ebcfa890ab3c0
Alexandre Gressier <alexandre@gressier.dev>
2021-10-13 17:50:14 +0200

    Create an intermediate MyApp component

--- app/src/main/java/dev/gressier/composebasics/MainActivity.kt
+++ app/src/main/java/dev/gressier/composebasics/MainActivity.kt
@@ -3,13 +3,17 @@ package dev.gressier.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.gressier.composebasics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,25 +21,34 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                Greeting("Android")
             }
         }
     }
 }
 
 @Composable
+fun MyApp(content: @Composable () -> Unit) {
+    ComposeBasicsTheme {
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        "Hello $name!",
+        Modifier.padding(16.dp)
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeBasicsTheme {
+    MyApp {
         Greeting("Android")
     }
 }
